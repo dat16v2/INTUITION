30b4b473c68eccb9720bdbbc236d4a0128c87916
Emil Hummel <sequoiia@hummel.yt>
2016-11-21 12:47:36 +0100

    Initial structure

---
+++ src/kea/intuition/Intuition.java
@@ -0,0 +1,10 @@
+package kea.intuition;
+
+import javafx.stage.Stage;
+
+public class Intuition {
+    public static void Initialize(Stage stage)
+    {
+        stage.setTitle("INTUITION");
+    }
+}
